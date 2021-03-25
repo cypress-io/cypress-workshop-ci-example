56109d2f78bb25ad18d0dae5b0146370d0a6a424
Anne <anne.fougere@hivency.com>
2021-03-25 17:54:20 +0100

    parrallel

---
+++ cypress/integration/spec-c.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+describe('Example site', () => {
+ 
+  it('goes directly to README', () => {
+    cy.visit('/README')
+    cy.contains('h2', 'README')
+  })
+
+})
