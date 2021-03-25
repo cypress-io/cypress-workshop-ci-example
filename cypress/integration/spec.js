6d483c37c7feac8dd6a69c9654907a976089b7a7
Daria Trainor <dariasu@gmail.com>
2021-03-26 03:20:08 +1100

    Upload screenshots and videos

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -9,7 +9,7 @@ describe('Example site', () => {
   it('navigates to README', () => {
     cy.visit('/')
     cy.contains('a', 'README').click()
-    cy.location('pathname').should('equal', '/READMEsadas/')
+    cy.location('pathname').should('equal', '/README/')
   })
 
   it('goes directly to README', () => {
