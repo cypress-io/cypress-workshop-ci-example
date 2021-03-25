11aa0d9a7056a43adec29f05e5e775057f5afe94
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-03-25 12:55:18 -0400

    add actual spec files

---
+++ cypress/integration/spec-d.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+
+describe('Example site', () => {
+  it('loads', () => {
+    cy.visit('/')
+    cy.contains('h1', 'cypress-workshop-ci-example').should('be.visible')
+  })
+
+  it('navigates to README', () => {
+    cy.visit('/')
+    cy.contains('a', 'README').click()
+    cy.location('pathname').should('equal', '/README/')
+  })
+
+  it('goes directly to README', () => {
+    cy.visit('/README')
+    cy.contains('h2', 'README')
+  })
+
+  it('redirects to README/', () => {
+    cy.visit('/README/')
+    cy.contains('h2', 'README')
+  })
+})
